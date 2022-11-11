2be3fdde234ad59c1577dc0214b0cceb8d54c840
LegendaryVasya <star.wars.vam.pis@gmail.com>
2022-11-11 21:24:43 +0300

    add task5 and fin 4

--- Lesson1_lvl1/Task4/Program.cs
+++ Lesson1_lvl1/Task4/Program.cs
@@ -2,6 +2,21 @@
 //а) с использованием третьей переменной;
 //б) *без использования третьей переменной.
 
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Программа меняет значения местами: ");
+//        Console.WriteLine("Введите значение a");
+//        int a = Convert.ToInt32(Console.ReadLine());
+//        Console.WriteLine("Введите значение b");
+//        int b = Convert.ToInt32(Console.ReadLine());
+//        int t = a; 
+//        Console.WriteLine($"a = {a=b}");
+//        Console.WriteLine($"b = {b = t}");
+//    }
+//}
+
 class Program
 {
     static void Main(string[] args)
@@ -11,8 +26,8 @@ class Program
         int a = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Введите значение b");
         int b = Convert.ToInt32(Console.ReadLine());
-        int t = a; 
-        Console.WriteLine($"a = {a=b}");
-        Console.WriteLine($"b = {b = t}");
+        a = a ^ b;
+        Console.WriteLine($"b = {b = b ^ a}");
+        Console.WriteLine($"a = {a = a ^ b}");
     }
 }
