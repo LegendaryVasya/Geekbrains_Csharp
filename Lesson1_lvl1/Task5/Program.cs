1f21968166d8f26603fddddb3a0104fb72fdd254
LegendaryVasya <star.wars.vam.pis@gmail.com>
2022-11-12 10:08:39 +0300

    add Task5 and 6

--- Lesson1_lvl1/Task5/Program.cs
+++ Lesson1_lvl1/Task5/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+//    б) *Сделать задание, только вывод организовать в центре экрана.
+//    в) **Сделать задание б с использованием собственных методов (например, Print(string ms, int
+//    x,int y).
+
+//using System;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Введите свое имя:");
+//        string name = Console.ReadLine();
+//        Console.WriteLine("Введите свое фамилия:");
+//        string secname = Console.ReadLine();
+//        Console.WriteLine("Введите свое город проживания:");
+//        string location = Console.ReadLine();
+//        string s = name + " " + secname + " " + location;
+//        Console.SetCursorPosition((Console.WindowWidth - s.Length) / 2, Console.CursorTop);
+//        Console.WriteLine(s);
+//    }
+//}
+
+using System;
+
+class Program
+{
+
+    static void Print(string msg)
+    {
+        Console.SetCursorPosition((Console.WindowWidth - msg.Length) / 2, Console.CursorTop);
+        Console.WriteLine(msg);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите свое имя:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Введите свое фамилия:");
+        string secname = Console.ReadLine();
+        Console.WriteLine("Введите свое город проживания:");
+        string location = Console.ReadLine();
+        string s = name + " " + secname + " " + location;
+        Print(s);
+    }
+}
---
+++ Lesson1_lvl1/Task6/ForStud.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class ForStud
+{
+    static void Pause()
+    {
+        Console.WriteLine("Pause?");
+        Thread.Sleep(10000);
+        Console.WriteLine("Yeah");
+    }
+
+    static void Print(string name, string secname, int age)
+    {
+        Console.WriteLine($"Имя: {name} Фамилия: {secname} Возраст: {age}");
+    }
+
+    static void Main(string[] args)
+    {
+        Pause();
+    }
+}
